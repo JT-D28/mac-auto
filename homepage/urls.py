d49a52fef3ca7ac8a5ecff10831905222bd225d3
陈汉源 <chenhanyuan@fingard.com>
2020-01-14 16:35:52 +0800

    pyechart注释

--- homepage/urls.py
+++ homepage/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('queryproduct/', views.queryproduct),
     path('queryplan/', views.queryplan),
     path('querytaskid/', views.querytaskid),
-    path('reportone/', views.reportone),
+    #path('reportone/', views.reportone),
 
 ]

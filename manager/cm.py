b5683030e0978282c0254c7b9bf5c55ca77bf956
陈汉源 <chenhanyuan@fingard.com>
2020-01-20 17:46:27 +0800

    邮箱钉钉配置bug

--- manager/cm.py
+++ manager/cm.py
@@ -187,27 +187,39 @@ def editplan(request):
 	id_=request.POST.get('uid').split('_')[1]
 	msg=''
 	try:
+		is_send_mail=request.POST.get("is_send_mail")
+		is_send_dingding=request.POST.get("is_send_dingding")
 		plan=Plan.objects.get(id=id_)
 		plan.description=request.POST.get('description')
 		plan.db_id=request.POST.get('dbid')
 		print('description=>',plan.description)
 		plan.run_type=request.POST.get('run_type')
-		mail_config_id=plan.mail_config_id
 		plan.save()
-
-		mail_config=MailConfig.objects.get(id=mail_config_id)
-		print(mail_config)
-		is_send_mail=request.POST.get("is_send_mail")
-		is_send_dingding=request.POST.get("is_send_dingding")
-		if is_send_mail=='true':
-			mail_config.is_send_mail='open'
+		print(plan.mail_config_id)
+		if plan.mail_config_id =='' or plan.mail_config_id is None :         #针对老任务，没有邮箱配置
+			mail_config = MailConfig()
+			if is_send_mail == 'true':
+				mail_config.is_send_mail = 'open'
+			else:
+				mail_config.is_send_mail = 'close'
+			if is_send_dingding == 'true':
+				mail_config.is_send_dingding = 'open'
+			else:
+				mail_config.is_send_dingding = 'close'
+			mail_config.save()
+			plan.mail_config_id=mail_config.id
+			plan.save()
 		else:
-			mail_config.is_send_mail='close'
-		if is_send_dingding == 'true':
-			mail_config.is_send_dingding = 'open'
-		else:
-			mail_config.is_send_dingding = 'close'
-		mail_config.save()
+			mail_config=MailConfig.objects.get(id=plan.mail_config_id)
+			if is_send_mail=='true':
+				mail_config.is_send_mail='open'
+			else:
+				mail_config.is_send_mail='close'
+			if is_send_dingding == 'true':
+				mail_config.is_send_dingding = 'open'
+			else:
+				mail_config.is_send_dingding = 'close'
+			mail_config.save()
 		msg='编辑成功'
 		return{
 		'status':'success',

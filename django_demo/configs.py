be08e5a1fddac727320f6a6823800d7c92b3a020
陈汉源 <chenhanyuan@fingard.com>
2020-02-14 14:09:49 +0800

    set

---
+++ django_demo/configs.py
@@ -0,0 +1,37 @@
+# 数据库类型  sqlite3|mysql
+dbtype = 'sqlite3'
+
+# ME2 url地址
+ME2_URL = '127.0.0.1:8000'
+
+# 环境数据库配置
+#mysql
+DATABASES_NAME = 'me2-test-mysql'
+DATABASES_USER = 'root'
+DATABASES_PWD = '123456'
+DATABASES_HOST = '10.60.44.59'
+DATABASES_PORT = '3306'
+
+#sqlite3 默认文件名为db.sqlite3
+
+
+# redis配置
+REDIS_HOST = '10.60.44.59'
+REDIS_PORT = '6379'
+REDIS_PASSWORD = ''
+
+# 邮件服务器配置
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = '1090233097@qq.com'  # 邮箱帐号
+EMAIL_HOST_PASSWORD = 'vgzevlmbpbmgfefd'  # 邮箱密码
+EMAIL_FROM = '1090233097@qq.com'  # 邮件发送者帐号
+
+
+# 初次启动时创建管理员账号  用户名默认：admin
+IS_CREATE_SUPERUSER = True
+SUPERUSER_PWD = 'admin#fingard'
+

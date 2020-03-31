15acd1f39a3f577df3785445204d7aa984c59e90
陈汉源 <chenhanyuan@fingard.com>
2020-03-31 14:40:09 +0800

    Update login/views.py

--- login/views.py
+++ login/views.py
@@ -55,7 +55,7 @@ def initDataupdate():
 	print('变量tag更新完成')
 	dbcons = DBCon.objects.all()
 	for dbcon in dbcons:
-		if dbcon.scheme is None:
+		if dbcon.scheme is None or dbcon.scheme=='':
 			dbcon.scheme = '全局'
 			dbcon.save()
 			time.sleep(0.001)

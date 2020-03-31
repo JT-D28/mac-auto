5c0bdfb91a2b264f23b383bec513b85a1ca835a0
陈汉源 <chenhanyuan@fingard.com>
2020-03-31 09:25:07 +0800

    字段修改

--- manager/pa.py
+++ manager/pa.py
@@ -190,7 +190,7 @@ class MessageParser(object):
             t.kind=tkws['kind']
             t.name=tkws['name']
             t.description=tkws['description']
-            t.content_url=tkws['content_url']
+            # t.content_url=tkws['content_url']
             t.save()
 
             return {

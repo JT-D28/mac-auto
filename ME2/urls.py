f7542322f66d23da3f437fe755cf14bb66080558
陈汉源 <chenhanyuan@fingard.com>
2020-04-09 10:17:06 +0800

    静态文件

--- ME2/urls.py
+++ ME2/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+
+from ME2.settings import STATIC_ROOT
 from login import views
 
 urlpatterns = [
+	url(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
 	path('admin/', admin.site.urls),
 	path('', views.index),
 	path('account/', include('login.urls')),
@@ -25,8 +30,8 @@ urlpatterns = [
 	path('homepage/', include('homepage.urls')),
 	path('test_expression/', views.testexpress),
 	path('test_expression1/', views.testexpress1),
-    path('test_expression2/', views.testexpress2),
-    path('test_xml/', views.testxml),
+	path('test_expression2/', views.testexpress2),
+	path('test_xml/', views.testxml),
 	# path('',views.index),
 
 ]

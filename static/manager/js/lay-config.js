52fabfbdf2a95a6bd365f27d249b58f9b1863dc6
陈汉源 <chenhanyuan@fingard.com>
2020-04-21 08:48:38 +0800

    表格编辑___变量管理

---
+++ static/manager/js/lay-config.js
@@ -0,0 +1,5 @@
+layui.config({
+    base: '/static/manager/js/layuiTableColumnEdit/'      //自定义layui组件的目录
+}).extend({                             //设定组件别名
+    layuiTableColumnEdit:'js/layuiTableColumnEdit'
+});

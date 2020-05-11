db2ec48005ee36f416f3ff6cb4c8f21a6b557c53
CHY <chenhanyuan@fingard.com>
2020-05-11 16:52:17 +0800

    fix

--- manager/invoker.py
+++ manager/invoker.py
@@ -698,8 +698,7 @@ async def dealruninfo(planid,taskid,info=None,startnodeid=''):
 		ROUND(CONCAT(success*100/total),1) AS rate FROM (SELECT sum(CASE WHEN result="success"
 		THEN 1 ELSE 0 END) AS success,sum(CASE WHEN result !="OMIT" THEN 1 ELSE 0 END) AS total
 		FROM manager_resultdetail WHERE taskid=%s) AS x''',[taskid])
-		if cursor.fetchone():
-			info['successnum'],info['total'],info['rate'] = cursor.fetchone()
+		info['successnum'],info['total'],info['rate'] = cursor.fetchone()
 
 			
 	data = {'root':[],'info':info}
